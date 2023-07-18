63e13477b06ee2d94342b1ac11595dc5cafd0485
hespnod <hespnod2348@gmail.com>
2023-07-19 01:05:11 +0530

    second committ for sharp

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ maintitle.style.border = 'solid 3px black';
 let title = document.querySelector('.title');
 title.style.color = 'green';
 title.style.fontWeight = 'bold';
+
+let items = document.getElementsByClassName('list-group-item');
+items[2].style.backgroundColor = 'green';
+
+Array.from(items).forEach(function (element) {
+    element.style.fontWeight = 'bold';
+})
