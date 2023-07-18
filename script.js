58b57d23b5919f8e0584cf6c66ec5cc054d7740d
hespnod <hespnod2348@gmail.com>
2023-07-19 00:29:40 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.title = "Sharpner project1";
+let maintitle = document.querySelector('#header-title');
+maintitle.style.border = 'solid 3px black';
+
+let title = document.querySelector('.title');
+title.style.color = 'green';
+title.style.fontWeight = 'bold';
