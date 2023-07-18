b49b48dc85b73e839d4f6a1d32deaa511399daee
hespnod <hespnod2348@gmail.com>
2023-07-19 01:20:45 +0530

    third commit without class in li

--- script.js
+++ script.js
@@ -6,9 +6,13 @@ let title = document.querySelector('.title');
 title.style.color = 'green';
 title.style.fontWeight = 'bold';
 
-let items = document.getElementsByClassName('list-group-item');
-items[2].style.backgroundColor = 'green';
+// let items = document.getElementsByClassName('list-group-item');
+// items[2].style.backgroundColor = 'green';
 
-Array.from(items).forEach(function (element) {
-    element.style.fontWeight = 'bold';
-})
+// Array.from(items).forEach(function (element) {
+//     element.style.fontWeight = 'bold';
+// })
+
+let li = document.getElementsByTagName('li');
+li[4].style.backgroundColor = 'green';
+li[4].style.color = 'white';
