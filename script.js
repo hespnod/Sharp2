ceebfa1fc75ff707dbd16fb6d115b3588c4c6a62
hespnod <hespnod2348@gmail.com>
2023-07-20 17:12:38 +0530

    added filtered functionality

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
-let ed = document.querySelectorAll('li');
+let listItems = document.querySelectorAll('li');
 
-let u = document.querySelector('ul');
+let ulContent = document.querySelector('ul');
 
-ed.forEach((ele, i) => {
-    let but = document.createElement('button');
-    but.innerText = 'Edit';
-    but.className = 'btn btn-danger btn-sm float-right';
-    ele.appendChild(but);
-
-    // u.appendChild(ele);  `
-})
+let filter = document.getElementById('filter');
 
 let form = document.getElementById('addForm');
+
+let description = document.getElementById('description');
+
+//Creating and adding edit button
+listItems.forEach((ele) => {
+    let btn = document.createElement('button');
+    btn.innerText = 'Edit';
+    btn.className = 'btn btn-primary btn-sm float-right';
+    ele.appendChild(btn);
+
+})
+
+
+
 form.addEventListener('submit', addItem);
 
 function addItem(e) {
@@ -19,13 +26,15 @@ function addItem(e) {
     // Creating ul to append the li in it
     let ul = document.querySelector('ul');
     //storing the value of form in a variable to append it into li
-    let cb = document.createTextNode(e.target.firstElementChild.value);
+    let submitText1 = document.createTextNode(`${e.target.firstElementChild.value} `);
+    let submitText2 = document.createTextNode(e.target[1].value);
     let li = document.createElement('li');
-    li.append(cb);
+    li.append(submitText1);
+    // li.append(" ");
+    li.append(submitText2);
     li.className = 'list-group-item';
-    // li.appendChild(but);
 
-    console.log('list valu=', li)
+
     //creating button and appending it into the li
     let btn = document.createElement('button');
     btn.className = 'btn btn-danger btn-sm float-right delete';
@@ -33,16 +42,34 @@ function addItem(e) {
     li.appendChild(btn);
     ul.appendChild(li);
     e.target.firstElementChild.value = '';
+    e.target[1].value = '';
+
+    //Creating edit button and appending in li
+    let editBtn = document.createElement('button');
+    editBtn.className = 'btn btn-primary btn-sm float-right';
+    editBtn.innerText = 'Edit';
+    li.appendChild(editBtn);
 }
 
-let items = document.querySelector('ul');
-items.addEventListener('click', remove);
+ulContent.addEventListener('click', remove);
 
 function remove(e) {
     if (e.target.classList.contains('delete')) {
-
-        items.removeChild(e.target.parentElement);
+        ulContent.removeChild(e.target.parentElement);
     }
 }
 
-console.log('list valu=', ed)
+filter.addEventListener('keyup', filterEvents);
+
+function filterEvents(e) {
+    let inputText = e.target.value.toLowerCase();
+    let lists = document.querySelectorAll('li');
+    lists.forEach(function (ele) {
+        if ((ele.firstChild.textContent.toLowerCase().indexOf(inputText) != -1) || (ele.firstChild.nextSibling.textContent.toLowerCase().indexOf(inputText) != -1)) {
+            ele.style.display = 'block';
+        } else {
+            ele.style.display = 'none';
+        }
+    })
+
+}
